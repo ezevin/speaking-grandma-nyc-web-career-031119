cc3a43eedb9f598b821a7373dad1d4726edac72f
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-07 22:56:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,9 +18,9 @@ def speak_to_grandma(phrase)
   
   if phrase == 'Hi Nana, how are you?'
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == 
-    puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == 'Hi!'
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == 'WHAT DID YOU EAT TODAY?'
     puts "NO, NOT SINCE 1938!"
   elsif phrase == 'WHAT DID YOU EAT TODAY?'
     puts "NO, NOT SINCE 1938!"
