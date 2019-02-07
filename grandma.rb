94532513680948c5eff452089bac467e63f9b6d4
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-07 22:55:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -21,7 +21,7 @@ def speak_to_grandma(phrase)
   elsif phrase == 
     puts "HUH?! SPEAK UP, SONNY!"
   elsif phrase == 
-    puts
+    puts "NO, NOT SINCE 1938!"
   else phrase == 
     puts "I LOVE YOU TOO PUMPKIN!"
   end
