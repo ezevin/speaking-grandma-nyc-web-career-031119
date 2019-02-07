54d814a49840f32a927f863a349080cc0d34b157
Elizabeth Zevin <ehzevin@gmail.com>
2019-02-07 22:58:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 
-def speak_to_grandma(phrase)
+def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
   
   if phrase == 'Hi Nana, how are you?'
     puts "HUH?! SPEAK UP, SONNY!"
